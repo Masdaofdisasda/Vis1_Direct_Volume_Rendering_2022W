d487303e2a0174160392b1556b3d60c71594ace0
Nicolas Eder <nicolas.eder.1998@gmail.com>
2022-10-21 17:21:16 +0200

    Histogram updates when switching file (animated)

--- js/histogram.js
+++ js/histogram.js
@@ -6,16 +6,14 @@ class Histogram {
     }
 
     update(volume) {
+        this.setBins(volume);
+
         if (this.chart === null) {
-            this.setup(volume);
+            this.setupChart();
             return;
         }
-        // TODO: update bins
-    }
-
-    setup(volume) {
-        this.setBins(volume);
-        this.setupD3(volume);
+        
+        this.updateChart();
     }
 
     setBins(volume) {
@@ -32,31 +30,36 @@ class Histogram {
             }
         });
         this.rawBins = rawBins;
-        
+
         let biggestBinSize = 0;
         rawBins.forEach(bin => {
-            if(bin > biggestBinSize)
+            if (bin > biggestBinSize)
                 biggestBinSize = bin;
         })
-        if(biggestBinSize === 0){
+        if (biggestBinSize === 0) {
             return;
         }
 
         let normalizedBins = [];
         for (let i = 0; i < rawBins.length; i++) {
-            normalizedBins[i] = (rawBins[i]/biggestBinSize)*200;
-            if(normalizedBins[i] > 0 && normalizedBins[i] < 1){
+            normalizedBins[i] = (rawBins[i] / biggestBinSize) * 200;
+            if (normalizedBins[i] > 0 && normalizedBins[i] < 1) {
                 normalizedBins[i] = 1;
             }
         }
         this.prettyBins = normalizedBins;
     }
 
-    setupD3() {
+    setupChart() {
         this.chart = d3.select("#tfContainer")
             .selectAll("div")
             .data(this.prettyBins)
             .enter().append("div")
+            .style("height", function (value) { return value + "px"; });
+    }
+
+    updateChart() {
+        this.chart.data(this.prettyBins).transition(1000)
             .style("height", function (value) { return value + "px"; });
     }
 }
