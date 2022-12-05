01fb94180610076d8b298a41574684e5746945da
Nicolas Eder <nicolas.eder.1998@gmail.com>
2022-12-05 12:38:16 +0100

    Backup before converting histogram to svg

--- js/histogram.js
+++ js/histogram.js
@@ -8,56 +8,54 @@ class Histogram {
     update(densityArray) {
         this.bins = d3.bin().thresholds(this.binAmount)(densityArray);
 
-        if (this.chart === null) {
+        if (this.chart === null)
             this.setupChart();
-            return;
-        }
-
-        this.updateChart();
+        else
+            this.updateChart();
     }
 
     setupChart() {
-        let svg = d3.select('#tfContainer')
-            .append('svg')
-            .attr('width', "100%")
-            .attr('height', 150);
+        // let svg = d3.select('#tfContainer')
+        //     .append('svg')
+        //     .attr('width', "100%")
+        //     .attr('height', 150);
 
-        let yScale = d3.scaleLinear().domain([0, 100]).range([0, 150]);
-        let yAxis = d3.axisLeft(yScale).ticks(10);
+        // let yScale = d3.scaleLinear().domain([0, 100]).range([0, 150]);
+        // let yAxis = d3.axisLeft(yScale).ticks(10);
 
-        svg.append("g")
-            .attr("class", "axis")
-            .call(yAxis)
-            .append("text")
-            .style("text-anchor", "middle")
-            .text("domain name");
+        // svg.append("g")
+        //     .attr("class", "axis")
+        //     .call(yAxis)
+        //     .append("text")
+        //     .style("text-anchor", "middle")
+        //     .text("domain name");
 
-        let binScale = this.getBinScale();
-        this.chart = d3.select("#tfContainer")
-            .append('div')
-            .classed('histogram', true)
+        let binScalerFunction = this.getBinScalerFunction();
+        this.chart = d3.select(".histogram")
             .selectAll("div")
             .data(this.bins)
             .join("div")
-            .style("height", function (value) { return binScale(value.length) + "px"; });
+            .style("height", function (value) { return binScalerFunction(value.length) + "px"; });
     }
 
     updateChart() {
-        let binScale = this.getBinScale();
+        let binScale = this.getBinScalerFunction();
         this.chart.data(this.bins)
             .transition(5000)
             .style("height", function (value) { return binScale(value.length) + "px"; });
     }
 
-    getBinScale() {
+    getBinScalerFunction() {
         let binMaxValue = this.sizeOfBiggestBin(this.bins);
         return d3.scaleLinear().domain([0, binMaxValue]).range([0, 200]);
     }
 
-    // exists, because "max" function does not work for some reason
-    sizeOfBiggestBin(arr) {
+    /**
+     * Exists, because "max" function does not work for some reason
+     */
+    sizeOfBiggestBin(bins) {
         let biggestNumber = 0;
-        arr.forEach(num => {
+        bins.forEach(num => {
             if (num.length > biggestNumber)
                 biggestNumber = num.length;
         })
